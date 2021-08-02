79cbd48de1bbe3a2c78f1aa2e8d54c1c8aec75f6
Andrews Antwi-Addo <59352495+cUpid-py@users.noreply.github.com>
2021-08-02 15:21:05 +0000

    Completed landing page and navbar sections

--- boltclone/src/App.js
+++ boltclone/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
+import Landingpage from "./components/landingpage"
 
 function App() {
   return (
     <div >
       <header>
+        <Navbar/>
+        <Landingpage/>
+      
       </header>
     </div>
   );
